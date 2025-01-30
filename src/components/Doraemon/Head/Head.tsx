a799a79e3057182c6513e1c3a652f77cddb0c4c4
gabit <gabit690@gmail.com>
2025-01-30 19:08:31 -0300

    feat: add takekoputa animation

--- src/components/Doraemon/Head/Head.tsx
+++ src/components/Doraemon/Head/Head.tsx
@@ -2,8 +2,8 @@ import head from '../../../../temp/doraemon-head-copy.jpg'
 
 const Head = () => {
   return (
-    <div className="w-80 h-60 outline outline-double outline-blue-800 mx-auto relative">
-      <img src={head} alt="head" className="opacity-50 temporal-image h-60" />
+    <div className="w-80 h-60 outline-double outline-blue-800 mx-auto relative">
+      <img src={head} alt="head" className="opacity-10 temporal-image h-60" />
     </div>
   )
 }
